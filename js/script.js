acf061d509f01891a84cfe7416ddbd49320e9200
Matthew <mattpsimpson@outlook.com>
2026-03-11 18:41:14 -0400

    Fixed issues with mobile display

--- js/script.js
+++ js/script.js
@@ -1,6 +1,13 @@
 const menuIcon = document.querySelector('#menu-icon');
 const navLinks = document.querySelector('.nav-links');
+const navItems = document.querySelectorAll(".nav-links a");
 
-menuIcon.onclick = () => {
-    navLinks.classList.toggle('active');
-}
+menuIcon.addEventListener("click", () => {
+    navLinks.classList.toggle("active");
+});
+
+navItems.forEach(link => {
+    link.addEventListener("click", () => {
+        navLinks.classList.remove("active");
+    });
+});
